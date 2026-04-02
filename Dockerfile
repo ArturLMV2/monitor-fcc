64f0f2a66bfe212aab04e03e822baf942038e6fa
ArturLMV2 <65974239+ArturLMV2@users.noreply.github.com>
2026-04-02 11:11:48 -0300

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.11-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+RUN apt-get update && apt-get install -y alsa-utils
+RUN apt-get update && apt-get install -y tzdata
+ENV TZ=America/Sao_Paulo
+COPY monitor.py .
+
+VOLUME ["/data"]
+
+CMD ["python", "monitor.py"]
---
+++ monitor.py
@@ -0,0 +1,106 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+import time
+from datetime import datetime
+import smtplib
+from email.mime.text import MIMEText
+
+URL = "https://www.concursosfcc.com.br/concursos/cpupe125/index.html"
+CACHE = "/data/cache.json"
+INTERVALO = 60  # 5 minutos
+def enviar_email(mensagem):
+    remetente = "arturluism@gmail.com"
+    senha = ""
+    destinatario = "arturluis_@hotmail.com, raylanemns@gmail.com"
+
+    msg = MIMEText(mensagem)
+    msg["Subject"] = "🚨 Nova publicação FCC"
+    msg["From"] = remetente
+    msg["To"] = destinatario
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(remetente, senha)
+        server.send_message(msg)
+def alerta_visual():
+    for _ in range(5):
+        print("\033[1;31m")
+        print("🚨🚨🚨 NOVA PUBLICAÇÃO DETECTADA 🚨🚨🚨")
+        print("\033[0m")
+        time.sleep(0.3)
+
+def beep():
+    open("/data/alert.txt", "w").close()
+
+def obter_publicacoes():
+    r = requests.get(URL, timeout=30)
+    r.raise_for_status()
+
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    itens = []
+
+    for a in soup.find_all("a"):
+        texto = a.get_text(strip=True)
+        href = a.get("href")
+
+        if not texto:
+            continue
+
+        if any(x in texto.lower() for x in ["edital", "resultado", "convocação", "retificação"]):
+            itens.append({
+                "texto": texto,
+                "href": href
+            })
+
+    return itens
+
+
+def carregar_cache():
+    if not os.path.exists(CACHE):
+        return []
+    with open(CACHE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def salvar_cache(data):
+    os.makedirs(os.path.dirname(CACHE), exist_ok=True)
+    with open(CACHE, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
+
+print("��� Monitor FCC iniciado...")
+print("��� Aguardando alterações...\n")
+
+while True:
+    try:
+        antigos = carregar_cache()
+        atuais = obter_publicacoes()
+
+        novos = [i for i in atuais if i not in antigos]
+
+        if novos:
+            mensagem = "\n\n".join(
+               f"{item['texto']}\n{item['href']}"
+               for item in novos
+            )
+            enviar_email(mensagem)
+            alerta_visual()
+            beep()
+            print("\n��������� NOVA PUBLICAÇÃO DETECTADA ���������")
+            print(f"⏰ {datetime.now()}\n")
+
+            for item in novos:
+                print(f"��� {item['texto']}")
+                print(f"��� {item['href']}\n")
+
+            salvar_cache(atuais)
+
+        else:
+            print(f"✔ Sem mudanças - {datetime.now()}")
+
+    except Exception as e:
+        print(f"❌ erro: {e}")
+
+    time.sleep(INTERVALO)
